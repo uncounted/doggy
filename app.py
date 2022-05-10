5916b572e5479bd800909cb55eab27a885867514
hoholoudly <hoholoudly@gmail.com>
2022-05-10 22:34:56 +0900

    로그인/회원가입/메인

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, render_template, request, jsonify, url_for, make_response
+import jwt
+import datetime
+import hashlib
+import certifi
+from pymongo import MongoClient
+from werkzeug.utils import secure_filename, redirect
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+SECRET_KEY = 'SPARTA'
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.7eo9i.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
+db = client.dbdoggy
+
+# 로그인 여부 체크(로그인이 필요한 모든 페이지에서 실행 필요)
+def checklogin():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+        return user_info['id']
+    except jwt.ExpiredSignatureError:
+        return "logout"
+    except jwt.exceptions.DecodeError:
+        return 'logout'
+
+@app.route('/')
+def home():
+    result = checklogin()
+    if result == 'logout':
+        return render_template("index.html", msg="logout")
+    else:
+        return render_template("index.html", userid=result)
+    # token_receive = request.cookies.get('mytoken')
+    # try:
+    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #     user_info = db.user.find_one({"id": payload['id']})
+    #     print(user_info['id'])
+    #     return render_template('index.html', userid=user_info['id'], msg='login')
+    # except jwt.ExpiredSignatureError:
+    #     return redirect(url_for("index.html", msg="logout"))
+    # except jwt.exceptions.DecodeError:
+    #     return render_template('index.html', msg='logout')
+
+@app.route('/login')
+def login():
+    result = checklogin()
+    if result == 'logout':
+        return render_template('login.html', msg='logout')
+    else:
+        return render_template('login.html', msg='login')
+
+@app.route('/join')
+def join():
+    result = checklogin()
+    if result == 'logout':
+        return render_template('join.html', msg='logout')
+    else:
+        return render_template('join.html', msg='login')
+
+@app.route('/logout')
+def logout():
+    return render_template('index.html', msg='logout')
+
+@app.route('/api/join', methods=['POST'])
+def api_join():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    name_receive = request.form['name_give']
+    dname_receive = request.form['dname_give']
+    dage_receive = request.form['dage_give']
+    dgender_receive = request.form['dgender_give']
+    dbirth_receive = request.form['dbirth_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'name': name_receive, 'dog_name':dname_receive, 'dog_age':dage_receive, 'dog_gender':dgender_receive, 'dog_birthday':dbirth_receive})
+
+    return jsonify({'result': 'success'})
+
+@app.route('/api/join/dup', methods=['POST'])
+def api_join_dup():
+    id_receive = request.form['id_give']
+    result = db.user.find_one({'id': id_receive})
+
+    if result:
+        return jsonify({'msg':'중복된 아이디가 있습니다'})
+    else:
+        return jsonify({'result': 'success'})
+
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    # 로그인
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': id_receive,
+         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/user.js
@@ -0,0 +1,116 @@
+$(function() {
+    $('#uid').focusout(function(){
+        $.ajax({
+            type: "POST",
+            url:"/api/join/dup",
+            data: {id_give: $('#uid').val()},
+            success: function(response){
+                if (response['msg']){
+                    $('#dup-chk').removeClass('is-hidden')
+                    $('#dup-chk').text(response['msg'])
+                } else {
+                    $('#dup-chk').addClass('is-hidden')
+                }
+            }
+        })
+    })
+})
+
+$(function() {
+    $('#upw').focusout(function() {
+        const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/;
+        const upwChk = regExp.test($('#upw').val())
+
+        if(!upwChk) {
+            $('#wrong-pw').removeClass('is-hidden')
+            $('#wrong-pw').text('비밀번호는 영문(대소문자구분),숫자,특수문자(~!@#$%^&*()-_? 만 허용)를 혼용하여 8~16자를 입력해주세요.')
+        } else {
+            $('#wrong-pw').addClass('is-hidden')
+        }
+    })
+})
+
+$(function() {
+    $('#upw2').focusout(function(){
+        if($('#upw').val() != $('#upw2').val()) {
+            $('#chk-pw').removeClass('is-hidden')
+            $('#chk-pw').text('비밀번호가 일치하지 않습니다')
+        } else {
+            $('#chk-pw').addClass('is-hidden')
+        }
+    })
+})
+
+function join() {
+    const uid = $('#uid').val()
+    const upw = $('#upw').val()
+    const username = $('#username').val()
+    const dogname = $('#dogname').val()
+    const dogage = $('#dogage').val()
+    const doggender = $('input[name="doggender"]:checked').val()
+    const dogbirth = $('#dogbirth').val()
+    const dupchk = $('#dup-chk').css('display')
+    const chkpw = $('#chk-pw').css('display')
+    const wrongpw = $('#wrong-pw').css('display')
+
+    if (!uid || !upw || !username  || !dogname || !dogage || !doggender || !dogbirth) {
+        alert("모든 항목을 입력해주세요")
+    } else if (dupchk !='none' || chkpw != 'none' || wrongpw != 'none'){
+        alert("입력 항목을 확인해주세요")
+    } else {
+            $.ajax({
+                type: "POST",
+                url: "/api/join",
+                data: {
+                    id_give: uid,
+                    pw_give: upw,
+                    name_give: username,
+                    dname_give: dogname,
+                    dage_give: dogage,
+                    dgender_give: doggender,
+                    dbirth_give: dogbirth
+                },
+                success: function (response) {
+                    if (response['result'] == 'success') {
+                        alert('회원가입이 완료되었습니다.')
+                        window.location.href = '/login'
+                    } else {
+                        alert(response['msg'])
+                    }
+                }
+            })
+    }
+}
+
+function login() {
+
+    if (!$('#userid').val()|| !$('#userpw').val()) {
+        alert ('아이디 또는 비밀번호를 입력하세요')
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/api/login",
+            data: {id_give: $('#userid').val(), pw_give: $('#userpw').val()},
+            success: function (response) {
+                console.log(response)
+                if (response['result'] == 'success') {
+                    // 로그인이 정상적으로 되면, 토큰을 받아옵니다.
+                    // 이 토큰을 mytoken이라는 키 값으로 쿠키에 저장합니다.
+                    $.cookie('mytoken', response['token']);
+
+                    alert('로그인 완료!')
+                    window.location.href = '/'
+                } else {
+                    // 로그인이 안되면 에러메시지를 띄웁니다.
+                    alert(response['msg'])
+                }
+            }
+        })
+    }
+}
+
+function logout() {
+    $.removeCookie('mytoken', {path: '/'});
+    alert('로그아웃!');
+    window.location.href = "/";
+}
